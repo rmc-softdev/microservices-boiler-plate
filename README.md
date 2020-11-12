59d9701ee81f3169cf997da34e61146ca037565c
Rafael Marques <rafael.marques.cavalcante@gmail.com>
2020-11-12 08:52:07 -0300

    rudiments of db connection in place

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # How to run it
 
-run npm run dev if you want to run TS, but to build the docker image, just run
+run npm run dev if you want to run TS, but to build the docker image, just run skaffold instead of doing kubectl apply and all that to each dpl.
 
 - skaffold dev
+
+# How to debug
+
+To check which pods are running:
+
+- kubectl get pods
 
 Notas: 0) Qualquer throw new ALGUMERRORCUSTOM é pego pelo nosso error-handler e interpretado apropriadamente.
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors'
 import { json } from 'body-parser';
+import mongoose from 'mongoose'
 
 import {CurrentUserRouter} from './routes/current-user'
 import {SignInRouter} from './routes/signin'
@@ -23,6 +24,25 @@ app.all('*', () => {
 })
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+const start = async () => {
+  
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+
+  console.log("connected to mongodb")
+
+  } catch (err) {
+    console.log(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+  
+}
+
+start()
